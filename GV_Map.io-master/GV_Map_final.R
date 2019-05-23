a9a2b0bd557536b1f4168c65faacc27eab3e86e8
Lisa Hirschfield <alisonporchnik@users.noreply.github.com>
2019-05-22 23:38:51 -0400

    Add files via upload

---
+++ GV_Map.io-master/GV_Map_final.R
@@ -0,0 +1,46 @@
+install.packages(data.table)
+require(data.table)
+require(rgdal)
+require(maptools)
+require(rgeos)
+require(maps)
+require(rgeos)
+require(sp)
+
+GV<-read.csv("GV_Map.csv")
+head(GV)
+
+GV$Long <- GV$LONGITUDE
+GV$Lat <- GV$LATITUDE
+GV$Name <-GV$NAME
+GV$Address <- GV$ADDRESS
+
+coords <- cbind(GV$Long, GV$Lat)
+Long <- as.numeric(GV$Long)
+Lat <- as.numeric(GV$Lat)
+
+require(sf)
+require(sp)
+library(shiny)
+require(shiny)
+library(magrittr)
+library(leaflet)
+require(leaflet)
+library(htmlwidgets)
+require(htmlwidgets)
+GV<-read.csv("GV_map.csv")
+head(GV)
+
+GV_map <- leaflet(data=GV) 
+GV_map <- leaflet(data=GV) %>%
+  addTiles(urlTemplate = "http://mapwarper.net/maps/tile/38310/{z}/{x}/{y}.png") %>%
+  addMarkers(~Long, ~Lat, 
+             popup = paste(
+                  "<img src =", GV$Image, " /><br>",
+                  " ", GV$Name, "<br>", 
+                  " ", GV$ID, "<br>","<br>", 
+                  " ", GV$About, "</div>"))
+                    
+GV_map
+saveWidget(GV_map, file="Bohemian_Greenwich_Village.html")
+                                            
---
+++ GV_Map.io-master/README.md
@@ -0,0 +1,4 @@
+<body><h1>Bohemian Greenwich Village</h1>
+<h2>A Historical Mapping Project</h2>
+<p>Lisa Hirschfield  May 2019</p>
+<br/>
